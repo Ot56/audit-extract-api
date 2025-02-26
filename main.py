86b7fe26a428f2bd626db93d84851f72e80421cc
Ot56 <o.elaffanii@gmail.com>
2025-02-26 21:13:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def extract_data():
     try:
         extracted_data = extract_audit_data(file_path)
         os.remove(file_path)  # Free up memory
-        return jsonify(extracted_data)
+        return jsonify(extracted_data, ensure_ascii=False)
 
     except Exception as e:
         import traceback
